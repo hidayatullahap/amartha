0a2327280532dd1aeaa3155238a95d44157de6f8
Hidayatullah Agung Prasetyo <hidayatullahap@gmail.com>
2026-02-12 22:48:45 +0700

    add todos

--- src/loans/controller/loan.go
+++ src/loans/controller/loan.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v5"
@@ -15,7 +16,23 @@ func NewLoanController() LoanController {
 
 func (r *LoanController) DecorateRoutes(e *echo.Echo) {
 	routeGroup := e.Group("/loans")
-	routeGroup.GET("", func(c *echo.Context) error {
-		return c.JSON(http.StatusOK, "Loan endpoint")
+	routeGroup.POST("", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, "[TODO] Create a new loan state: proposed")
+	})
+	routeGroup.POST("/:id/approve", func(c *echo.Context) error {
+		id := c.Param("id")
+		return c.JSON(http.StatusOK, fmt.Sprintf("[TODO] Approve loan id: %s", id))
+	})
+	routeGroup.POST("/:id/invest", func(c *echo.Context) error {
+		id := c.Param("id")
+		return c.JSON(http.StatusOK, fmt.Sprintf("[TODO] Invest loan id: %s", id))
+	})
+	routeGroup.POST("/:id/disburse", func(c *echo.Context) error {
+		id := c.Param("id")
+		return c.JSON(http.StatusOK, fmt.Sprintf("[TODO] Disburse loan id: %s", id))
+	})
+	routeGroup.GET("/:id", func(c *echo.Context) error {
+		id := c.Param("id")
+		return c.JSON(http.StatusOK, fmt.Sprintf("[TODO] Loan details for id: %s", id))
 	})
 }
