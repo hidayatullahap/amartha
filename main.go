9433264c7f54999c9174f91098dbd5c83b0d1647
Hidayatullah Agung Prasetyo <hidayatullahap@gmail.com>
2026-02-12 19:56:41 +0700

    add echo

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
 
 func main() {
-	fmt.Println("test")
+	e := echo.New()
+
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	if err := e.Start(":8080"); err != nil {
+		slog.Error("failed to start server", "error", err)
+	}
+}
+
+func hello(c *echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
