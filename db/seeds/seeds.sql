c653ab179e8bd1d7d7d0f49ebc9447501f80fd0e
Hidayatullah Agung Prasetyo <hidayatullahap@gmail.com>
2026-02-13 21:04:03 +0700

    typo email

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -1,6 +1,5 @@
 insert into users (id, username, role, email, created_at)
 values  (1, 'user1', 'user', 'user1@mail.com','2026-02-13 15:54:46.000'),
-        (2, 'user2', 'user', 'use2@mail.com','2026-02-13 15:54:48.000'),
+        (2, 'user2', 'user', 'user2@mail.com','2026-02-13 15:54:48.000'),
         (3, 'user3', 'user', 'user3@mail.com','2026-02-13 15:54:49.000'),
         (4, 'admin', 'admin','admin@mail.com','2026-02-13 15:54:49.000');
-        
