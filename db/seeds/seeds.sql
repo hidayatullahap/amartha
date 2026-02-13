e2b06af1e48c9f402ce9d3d52e9af5ab9fecc6cd
Hidayatullah Agung Prasetyo <hidayatullahap@gmail.com>
2026-02-13 21:03:54 +0700

    add email

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -1,5 +1,6 @@
-insert into users (id, username, role, created_at)
-values  (1, 'user1', 'user', '2026-02-13 15:54:46.000'),
-        (2, 'user2', 'user', '2026-02-13 15:54:48.000'),
-        (3, 'admin', 'admin', '2026-02-13 15:54:49.000'),
-        (4, 'user3', 'user', '2026-02-13 15:54:49.000');
+insert into users (id, username, role, email, created_at)
+values  (1, 'user1', 'user', 'user1@mail.com','2026-02-13 15:54:46.000'),
+        (2, 'user2', 'user', 'use2@mail.com','2026-02-13 15:54:48.000'),
+        (3, 'user3', 'user', 'user3@mail.com','2026-02-13 15:54:49.000'),
+        (4, 'admin', 'admin','admin@mail.com','2026-02-13 15:54:49.000');
+        
